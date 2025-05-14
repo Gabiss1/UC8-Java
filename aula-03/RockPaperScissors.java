483463b6ea0abf56be700c3e67340b75a0c55727
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-05-13 21:53:30 -0300

    Add files via upload

---
+++ aula-03/RockPaperScissors.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rockpaperscissors;
+
+/**
+ *
+ * @author GABRIELGARCEZDEOLIVE
+ */
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        Duel duel = new Duel();
+        duel.choose();
+    }
+}
