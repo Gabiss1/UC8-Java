c0db98fbd1452796d76e7032f2060de5b109b511
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-05-20 21:57:21 -0300

    Add files via upload

---
+++ aula-04/ExercicioArray.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.array;
+import java.util.Scanner;
+
+/**
+ *
+ * @author GABRIELGARCEZDEOLIVE
+ */
+public class ExercicioArray {
+    
+    public void Informacoes(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu e-mail: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite seu Senha: ");
+        String email = sc.nextLine();
+        
+        Usuario user = new Usuario(nome, email); 
+        this.criarArray(user);
+}
+    
+    public String[] criarArray(Usuario usuario){  
+        String[] infos = {usuario.getEmail(), usuario.getSenha()};
+        
+        System.out.println("\nInformações do Usuário:");
+        for(int i = 0; i < infos.length; i++){
+            System.out.println(infos[i]);
+        }
+        return infos;
+    }
+}
