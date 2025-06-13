5a86c89e2fc6db22aec272e808ecc3d29ffb1150
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-06-12 21:51:27 -0300

    Add files via upload

---
+++ aula-11/Rpg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rpg;
+
+/**
+ *
+ * @author GABRIELGARCEZDEOLIVE
+ */
+public class Rpg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
