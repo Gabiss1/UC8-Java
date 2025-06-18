08d2d800f8f0fdff2a1b0ea0adcf2f2d60766a79
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-06-17 21:52:13 -0300

    Add files via upload

---
+++ aula-15/Pokemon.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemonjlist;
+
+/**
+ *
+ * @author GABRIELGARCEZDEOLIVE
+ */
+public class Pokemon {
+    private String name;
+    private String type;
+    private int level;
+
+    public Pokemon(String name, String type, int level) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+    
+    public String getPoke(){
+        return name;
+    }
+}
