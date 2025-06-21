e555712645099b59f8b4519c3a691668a8930da1
Gabriel Oliveira <136657181+Gabiss1@users.noreply.github.com>
2025-06-20 21:47:24 -0300

    Add files via upload

---
+++ aula-16/Student.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package harrypotterjtable;
+
+/**
+ *
+ * @author GABRIELGARCEZDEOLIVE
+ */
+public class Student {
+    private String nome;
+    private String personalidade;
+
+    public Student(String nome, String personalidade) {
+        this.nome = nome;
+        this.personalidade = personalidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPersonalidade() {
+        return personalidade;
+    }
+    
+    
+}
